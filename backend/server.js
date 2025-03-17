4a59d02ffa95f06a2c15372db334d3d29912467f
Gayathri-Gogireddy <143862109+Gayathri-Gogireddy@users.noreply.github.com>
2025-03-17 16:34:15 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,14 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    res.send("MERN Backend is running!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
 
