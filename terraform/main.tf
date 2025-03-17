09468f1f1417300ce9cfbbbc1a4ecc859a083197
Gayathri-Gogireddy <143862109+Gayathri-Gogireddy@users.noreply.github.com>
2025-03-17 16:31:43 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,14 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_eks_cluster" "mern_cluster" {
+  name = "mern-cluster"
+  role_arn = aws_iam_role.eks_role.arn
+}
+
+resource "aws_iam_role" "eks_role" {
+  name = "eks-cluster-role"
+  managed_policy_arns = ["arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"]
+}
 
