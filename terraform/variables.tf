0508f294fac454185f75fa5e2d60377f16183209
Gayathri-Gogireddy <143862109+Gayathri-Gogireddy@users.noreply.github.com>
2025-03-17 16:36:55 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,26 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
 
+variable "cluster_name" {
+  description = "EKS Cluster Name"
+  type        = string
+  default     = "mern-cluster"
+}
+
+variable "s3_bucket" {
+  description = "S3 bucket for Terraform state"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID for EKS"
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "Subnets for EKS nodes"
+  type        = list(string)
+}
